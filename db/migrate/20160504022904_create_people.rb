b430d35555b5898bedc65dd8e192438d0f19b306
Oscar Rieken <oscar_rieken@homedepot.com>
2016-05-04 23:33:44 -0400

    got the memory db under control

--- db/migrate/20160504022904_create_people.rb
+++ db/migrate/20160504022904_create_people.rb
@@ -1,4 +1,8 @@
-class CreatePeople < ActiveRecord::Migration
+class CreatePerson < ActiveRecord::Migration
   def change
+    create_table :people do |t|
+      t.string :first_name
+      t.timestamps
+    end
   end
 end
--- db/migrate/20160504022910_create_friends.rb
+++ db/migrate/20160504022910_create_friends.rb
@@ -1,4 +1,9 @@
 class CreateFriends < ActiveRecord::Migration
   def change
+    create_table :friends do |t|
+      t.string :first_name
+      t.integer :person_id
+      t.timestamps
+    end
   end
 end
